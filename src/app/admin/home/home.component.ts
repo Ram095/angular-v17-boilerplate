060aafe1417f7aa841024f66e29ea3d99c8a1225
Sitaram <sitaram075@gmail.com>
2024-03-31 17:15:47 +0530

    eslint json and other ag-grid functionalities added

--- src/app/admin/home/home.component.ts
+++ src/app/admin/home/home.component.ts
@@ -1,66 +1,39 @@
-import { DecimalPipe, AsyncPipe, CommonModule } from '@angular/common';
+import { DecimalPipe } from '@angular/common';
 import { Component, PipeTransform } from '@angular/core';
-import { ReactiveFormsModule, FormControl } from '@angular/forms';
-import { NgbHighlight } from '@ng-bootstrap/ng-bootstrap';
+import { FormControl } from '@angular/forms';
 import { Observable, startWith, map } from 'rxjs';
-
+import data from '../../../assets/data/countries.json';
 interface Country {
   name: string;
   flag: string;
   area: number;
   population: number;
 }
-
-const COUNTRIES: Country[] = [
-  {
-    name: 'Russia',
-    flag: 'f/f3/Flag_of_Russia.svg',
-    area: 17075200,
-    population: 146989754,
-  },
-  {
-    name: 'Canada',
-    flag: 'c/cf/Flag_of_Canada.svg',
-    area: 9976140,
-    population: 36624199,
-  },
-  {
-    name: 'United States',
-    flag: 'a/a4/Flag_of_the_United_States.svg',
-    area: 9629091,
-    population: 324459463,
-  },
-  {
-    name: 'China',
-    flag: 'f/fa/Flag_of_the_People%27s_Republic_of_China.svg',
-    area: 9596960,
-    population: 1409517397,
-  },
-];
-
-function search(text: string, pipe: PipeTransform): Country[] {
-  return COUNTRIES.filter((country) => {
-    const term = text.toLowerCase();
-    return (
-      country.name.toLowerCase().includes(term) ||
-      pipe.transform(country.area).includes(term) ||
-      pipe.transform(country.population).includes(term)
-    );
-  });
-}
-
 @Component({
   selector: 'app-landing',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  Countries: Country[] = data;
+
   countries$: Observable<Country[]>;
   filter = new FormControl('', { nonNullable: true });
   constructor(pipe: DecimalPipe) {
     this.countries$ = this.filter.valueChanges.pipe(
       startWith(''),
-      map((text) => search(text, pipe))
+      map((text) => this.search(text, pipe))
     );
+  }
+
+  search(text: string, pipe: PipeTransform): Country[] {
+    return this.Countries.filter((country) => {
+      const term = text.toLowerCase();
+      return (
+        country.name.toLowerCase().includes(term) ||
+        pipe.transform(country.area).includes(term) ||
+        pipe.transform(country.population).includes(term)
+      );
+    });
   }
 }
--- src/app/admin/page-one/page-one.component.ts
+++ src/app/admin/page-one/page-one.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
 import {
   FormBuilder,
   FormControl,
   FormGroup,
   Validators,
 } from '@angular/forms';
-import { ColDef } from 'ag-grid-community';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ICellRendererAngularComp } from 'ag-grid-angular';
+import { ColDef, ICellRendererParams } from 'ag-grid-community';
+import { GridOptions } from 'ag-grid-community';
+
+@Component({
+  standalone: true,
+  template: `<div class="row">
+    <div (click)="DeleteRow()" class="col-6 icon">
+      <i class="bi bi-trash"></i>
+    </div>
+    <br />
+    <div (click)="EditRow()" class="col-6 icon">
+      <i class="bi bi-pencil"></i>
+    </div>
+  </div>`,
+})
+export class ActionButtonComponent implements ICellRendererAngularComp {
+  params!: ICellRendererParams;
+  agInit(params: ICellRendererParams): void {
+    this.params = params;
+  }
+  refresh(params: ICellRendererParams) {
+    this.params = params;
+    return true;
+  }
+  DeleteRow() {
+    this.params.context.componentParent.onDeleteRecord(this.params.data);
+  }
+  EditRow() {
+    this.params.context.componentParent.onEditRecord(this.params.data);
+  }
+}
+
+export interface ICarDetail {
+  make: string;
+  model: string;
+  price: number;
+  electric: boolean;
+  country: string;
+}
 
 @Component({
   selector: 'app-page-one',
@@ -15,7 +55,13 @@ import { ColDef } from 'ag-grid-community';
 export class PageOneComponent {
   gridApi: any;
   carDetailsForm: FormGroup;
-  apiResponse: any = [
+  closeResult = '';
+  selectedRecord: any;
+  toggleEditButton = false;
+  @ViewChild('confirmDeleteModal', { static: true }) deleteTemplate: any;
+  @ViewChild('carDetails', { static: true }) detailTemplate: any;
+
+  apiResponse: ICarDetail[] = [
     {
       make: 'Tesla',
       model: 'Model Y',
@@ -44,16 +90,209 @@ export class PageOneComponent {
       electric: true,
       country: 'USA',
     },
+    {
+      make: 'BMW',
+      model: '3 Series',
+      price: 40450,
+      electric: false,
+      country: 'Germany',
+    },
+    {
+      make: 'Audi',
+      model: 'Q5',
+      price: 43600,
+      electric: false,
+      country: 'Germany',
+    },
+    {
+      make: 'Nissan',
+      model: 'Leaf',
+      price: 31400,
+      electric: true,
+      country: 'Japan',
+    },
+    {
+      make: 'Chevrolet',
+      model: 'Silverado',
+      price: 28500,
+      electric: false,
+      country: 'USA',
+    },
+    {
+      make: 'Honda',
+      model: 'Civic',
+      price: 22150,
+      electric: false,
+      country: 'Japan',
+    },
+    {
+      make: 'Hyundai',
+      model: 'Kona',
+      price: 21350,
+      electric: true,
+      country: 'South Korea',
+    },
+    {
+      make: 'Volkswagen',
+      model: 'Passat',
+      price: 26390,
+      electric: false,
+      country: 'Germany',
+    },
+    {
+      make: 'Volvo',
+      model: 'XC90',
+      price: 49500,
+      electric: false,
+      country: 'Sweden',
+    },
+    {
+      make: 'Mazda',
+      model: 'CX-5',
+      price: 27440,
+      electric: false,
+      country: 'Japan',
+    },
+    {
+      make: 'Kia',
+      model: 'Sorento',
+      price: 29090,
+      electric: false,
+      country: 'South Korea',
+    },
+    {
+      make: 'Subaru',
+      model: 'Outback',
+      price: 26745,
+      electric: false,
+      country: 'Japan',
+    },
+    {
+      make: 'Lexus',
+      model: 'RX',
+      price: 45170,
+      electric: false,
+      country: 'Japan',
+    },
+    {
+      make: 'Jeep',
+      model: 'Wrangler',
+      price: 28765,
+      electric: false,
+      country: 'USA',
+    },
+    {
+      make: 'Land Rover',
+      model: 'Range Rover',
+      price: 92450,
+      electric: false,
+      country: 'UK',
+    },
+    {
+      make: 'Porsche',
+      model: '911',
+      price: 101200,
+      electric: false,
+      country: 'Germany',
+    },
+    {
+      make: 'GMC',
+      model: 'Sierra',
+      price: 30100,
+      electric: false,
+      country: 'USA',
+    },
+    {
+      make: 'Ram',
+      model: '1500',
+      price: 33200,
+      electric: false,
+      country: 'USA',
+    },
+    {
+      make: 'Chrysler',
+      model: 'Pacifica',
+      price: 35995,
+      electric: false,
+      country: 'USA',
+    },
+    {
+      make: 'Buick',
+      model: 'Encore',
+      price: 24500,
+      electric: false,
+      country: 'USA',
+    },
+    {
+      make: 'Jaguar',
+      model: 'F-Pace',
+      price: 51600,
+      electric: false,
+      country: 'UK',
+    },
+    {
+      make: 'Infiniti',
+      model: 'QX50',
+      price: 38650,
+      electric: false,
+      country: 'Japan',
+    },
+    {
+      make: 'Mitsubishi',
+      model: 'Outlander',
+      price: 26745,
+      electric: false,
+      country: 'Japan',
+    },
+    {
+      make: 'Acura',
+      model: 'MDX',
+      price: 47200,
+      electric: false,
+      country: 'Japan',
+    },
+    {
+      make: 'Lincoln',
+      model: 'Navigator',
+      price: 77090,
+      electric: false,
+      country: 'USA',
+    },
+    {
+      make: 'Cadillac',
+      model: 'Escalade',
+      price: 79495,
+      electric: false,
+      country: 'USA',
+    },
+    {
+      make: 'Fiat',
+      model: '500X',
+      price: 25895,
+      electric: false,
+      country: 'Italy',
+    },
   ];
 
   // Column Definitions: Defines the columns to be displayed.
   colDefs: ColDef[] = [
-    { field: 'make' },
-    { field: 'model' },
+    { field: '', checkboxSelection: true, width: 50 },
+    { field: 'make', filter: true, floatingFilter: true },
+    { field: 'model', filter: true, floatingFilter: true },
     { field: 'price' },
-    { field: 'electric' },
-    { field: 'country' },
+    { field: 'electric', width: 150 },
+    { field: 'country', filter: true, floatingFilter: true },
+    {
+      field: 'action',
+      width: 150,
+      cellRenderer: ActionButtonComponent,
+      filter: false,
+      sortable: false,
+    },
   ];
+
+  public paginationPageSize = 10;
+  public paginationPageSizeSelector: number[] | boolean = [10, 25, 50];
 
   get makeControl(): FormControl {
     return this.carDetailsForm.get('make') as FormControl;
@@ -71,19 +310,99 @@ export class PageOneComponent {
     return this.carDetailsForm.get('country') as FormControl;
   }
 
-  constructor(private fb: FormBuilder) {
+  get registerFormControl() {
+    return this.carDetailsForm.controls;
+  }
+
+  constructor(private fb: FormBuilder, private modalService: NgbModal) {
     this.carDetailsForm = this.fb.group({
-      make: this.fb.control('', [Validators.required]),
+      make: this.fb.control('', Validators.required),
       model: this.fb.control('', [Validators.required]),
       price: this.fb.control('', [Validators.required]),
-      electric: this.fb.control('', [Validators.required]),
+      electric: this.fb.control(false),
       country: this.fb.control('', [Validators.required]),
     });
   }
+
+  gridOptions = <GridOptions>{
+    context: {
+      componentParent: this,
+    },
+  };
 
   onGridReady(params: any) {
     this.gridApi = params?.api;
   }
 
-  openAddModal() {}
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'Reason was escape press';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'Backdrop was clicked';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+  openAddModal(content: TemplateRef<any>) {
+    this.toggleEditButton = false;
+    this.carDetailsForm.reset();
+    const modalRef = this.modalService.open(content);
+    modalRef.result.then((reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  saveCarDetails() {
+    if (this.carDetailsForm.valid) {
+      const newRecord = this.carDetailsForm.value;
+      this.apiResponse.push({ ...newRecord });
+      this.apiResponse = JSON.parse(JSON.stringify(this.apiResponse));
+      this.modalService.dismissAll('saved');
+    }
+  }
+
+  onDeleteRecord(param: ICarDetail) {
+    let newResponse: ICarDetail[] = [];
+    this.selectedRecord = param;
+    this.modalService.open(this.deleteTemplate).result.then(
+      (result) => {
+        this.closeResult = `Closed with: ${this.getDismissReason({ result })}`;
+
+        this.apiResponse.forEach((rec: ICarDetail) => {
+          if (rec.make !== this.selectedRecord.make) {
+            newResponse.push(rec);
+          }
+        });
+
+        this.apiResponse = [...newResponse];
+      },
+      (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason({ reason })}`;
+      }
+    );
+  }
+
+  onEditRecord(param: ICarDetail) {
+    this.selectedRecord = param;
+    this.carDetailsForm.patchValue(this.selectedRecord);
+    this.toggleEditButton = true;
+    this.modalService.open(this.detailTemplate).result.then((reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason({ reason })}`;
+    });
+  }
+
+  editCarDetails() {
+    if (this.carDetailsForm.valid) {
+      const updatedValue = this.carDetailsForm.value;
+      let existingRecordIndex = this.apiResponse.findIndex(
+        (item: ICarDetail) =>
+          item.make === this.selectedRecord.make &&
+          item.model === this.selectedRecord.model
+      );
+      this.apiResponse[existingRecordIndex] = updatedValue;
+      this.apiResponse = JSON.parse(JSON.stringify(this.apiResponse));
+      this.modalService.dismissAll('updated');
+    }
+  }
 }
